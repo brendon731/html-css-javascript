f1c01a78477c72c5edf70e7a81e4d7cede4200a0
unknown <brendon_gh@hotmail.com>
2022-05-18 13:24:58 -0300

    modularizando a aplicaçao

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
+import {projects} from "./projectsNames.js"
+const iframe = document.querySelector("iframe")
+const ul = document.querySelector("ul")
+let link = "<h3 class=title>projetos</h3>"
 
-    const iframe = document.querySelector("iframe")
-    const ul = document.querySelector("ul")
-    let link = "<h3 class=title>projetos</h3>"
-
-    function setIframe(element){
-        let a = document.querySelector(".current")
-        if(a){
-            a.classList.remove("current")
-        }
-        element.classList.add("current")
-        let path = element.id
-        iframe.src = `projects/${path}/${path}.html`
-       
+window.setIframe = (element) => {
+    let a = document.querySelector(".current")
+    if(a){
+        a.classList.remove("current")
     }
-
-    fetch("projects",
-        {headers:{ 
-            'Accept': 'application/json'
-        }}
-    )
-    .then(res=>res.json())
-    .then(r=>{
-        console.log(r)
-        r.forEach((element, index) => {
-            link += `
-            <li>
-                <a href="#" id=${element} onclick="setIframe(this)">
-                    ${element}
-                </a>
-            </li>
-            `        
-        })
-        ul.insertAdjacentHTML("beforeend", link)
-    })
+    element.classList.add("current")
+    let path = element.id
+    iframe.src = `projects/${path}/${path}.html`
     
-    .catch(error=>{console.log(error)})
+}
 
-    const menu = document.querySelector("#menu")
-    menu.onclick = e =>{ul.classList.toggle("nav--hidden")}
+const menu = document.querySelector("#menu")
+menu.onclick = e =>{ul.classList.toggle("nav--hidden")}
+
+
+projects.forEach((element, index) => {
+    link += `
+    <li>
+        <a href="#" id=${element} onclick="setIframe(this)">
+            ${element}
+        </a>
+    </li>
+    `        
+})
+ul.insertAdjacentHTML("beforeend", link)
